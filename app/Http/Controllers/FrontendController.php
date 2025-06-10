f65bd6fd45deae5741ba0786a1eb69b6e878cffc
maris <marisbalcuns123@gmail.comn>
2025-06-10 04:41:11 +0300

    subcategoryList

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -39,10 +39,23 @@ class FrontendController extends Controller
 
  public function findBasedOnChildcategory($categorySlug,
         Subcategory $subcategorySlug,
-        Childcategory $childCategorySlug,){
-        $advertisements = $childCategorySlug->ads;
+        Childcategory $childCategorySlug,
+        Request $request){
+
+        $advertisementBasedOnFilter = Advertisement::where(
+            'childcategory_id',
+            $childCategorySlug->id
+        )->when($request->minPrice, function ($query, $minPrice) {
+            return $query->where('price', '>=', $minPrice);
+        })->when($request->maxPrice, function ($query, $maxPrice) {
+            return $query->where('price', '<=', $maxPrice);
+        })->get();
+        $advertisementWithoutFilter = $childCategorySlug->ads;
 
          $filterByChildCategories = $subcategorySlug->ads->unique('childcategory_id');
+
+         $advertisements = $request->minPrice || $request->maxPrice ?
+        $advertisementBasedOnFilter : $advertisementWithoutFilter;
         return view(
     'product.childcategory',
     compact('advertisements', 
